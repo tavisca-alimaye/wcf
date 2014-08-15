a9a5f8ea779bd6139c3b8a33f57aef56d25c9dbb
Arnav Limaye <aal4493@gmail.com>
2014-08-15 13:48:47 +0530

    Started the wcf assignment

---
+++ EmployeeManager/EmployeeManager/ICreateEmployee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeManager
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICreateEmployee
+    {
+
+        [OperationContract]
+        void AddEmployee();
+
+        [OperationContract]
+        void AddRemark(Employee e);
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public string EmployeeID { get; set; }
+
+        [DataMember]
+        public string EmployeeName { get; set; }
+
+        [DataMember]
+        public Remarks remark;
+        
+        
+    }
+
+    public class Remarks
+    {
+        public DateTime RemarkDateTimeStamp { get; set; }
+        public string RemarkDescription { get; set; }
+        public Remarks()
+        {
+            RemarkDateTimeStamp = DateTime.Now;
+            RemarkDescription = null;
+        }
+    }
+}
